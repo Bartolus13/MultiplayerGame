9a0c8c7c5e3fa28eff638f8b6007e0b27f41f54d
Bartolus13 <bartosz.zawadzki13@gmail.com>
2025-04-09 20:20:20 +0200

    Dobra, dzis poszlo giga git, kolko i krzyzyk dziala

--- game.php
+++ game.php
@@ -1,5 +1,20 @@
 <?php
+
     $conn = mysqli_connect("localhost" ,"root", "", "gra");
+    $id_gracza = $_COOKIE["idGracza"];
+    $id_pomieszczenia = $_COOKIE["idPomieszczenia"];
+    $sql = "SELECT id_gracza1, id_gracza2 FROM pomieszczenia WHERE id = $id_pomieszczenia";
+    $gracze = mysqli_fetch_row(mysqli_query($conn, $sql));
+    if ($gracze[0] == $id_gracza) {
+        $id_gracza_2 = $gracze[1];
+    } else {
+        $id_gracza_2 = $gracze[0];
+    }
+
+    
+
+    
+    
 ?>
 
 <!DOCTYPE html>
@@ -8,15 +23,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gra Multiplayer</title>
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
-
-    <?php   
-            $sql = "SELECT id FROM gracze ORDER BY id DESC LIMIT 1";
-            $playerID = mysqli_fetch_row(mysqli_query($conn, $sql));
-            echo $playerID[0];
-    ?>
+    <header> 
+        <h1>Gra Multiplayer</h1>
+        <h2>Witaj w grze!</h2>
+        <p>Twoje ID gracza: <?php echo $id_gracza; ?></p>
+        <p>Twoje ID pomieszczenia: <?php echo $id_pomieszczenia; ?></p>
+    </header>
     
+    
+    <!-- Robimy grę kółko i krzyżyk -->
+    <h3>Kółko-krzyżyk</h3>
+    <div id="plansza">
+        <form action= "game.php" method="post">    
+        <input type="submit" id="przycisk0" name="przycisk" value="0">
+        <input type="submit" id="przycisk1" name="przycisk" value="1">
+        <input type="submit" id="przycisk2" name="przycisk" value="2"><br>
+        <input type="submit" id="przycisk3" name="przycisk" value="3">
+        <input type="submit" id="przycisk4" name="przycisk" value="4">
+        <input type="submit" id="przycisk5" name="przycisk" value="5"><br>
+        <input type="submit" id="przycisk6" name="przycisk" value="6">
+        <input type="submit" id="przycisk7" name="przycisk" value="7">
+        <input type="submit" id="przycisk8" name="przycisk" value="8"><br>
+        </form>
+    </div>
+
+    <a href="index.php">WRACAJ</a>
+
+    <?php
+
+         $sql = "SELECT plansza FROM pomieszczenia WHERE id = $id_pomieszczenia";
+         $plansza = mysqli_fetch_row(mysqli_query($conn, $sql))[0];
+         for ($i = 0; $i < 9; $i++) {
+             if ($plansza[$i] == "O") {
+                 echo "<script> document.getElementById('przycisk$i').setAttribute('value','O')</script>";
+                 echo "<script> document.getElementById('przycisk$i').setAttribute('disabled','true')</script>";
+             } else if ($plansza[$i] == "X") {
+                 echo "<script> document.getElementById('przycisk$i').setAttribute('value','X')</script>";
+                 echo "<script> document.getElementById('przycisk$i').setAttribute('disabled','true')</script>";
+             }
+         }
+
+
+        function klikPrzycisku($nrPrzycisku) {
+            global $conn, $id_gracza, $id_gracza_2, $id_pomieszczenia;
+            $sql = "SELECT tura FROM pomieszczenia WHERE id = $id_pomieszczenia";
+            $tura = mysqli_fetch_row(mysqli_query($conn, $sql))[0];
+            $iloscGraczy = mysqli_fetch_row(mysqli_query($conn, "SELECT liczba_graczy FROM pomieszczenia WHERE id = $id_pomieszczenia"))[0];
+            
+        
+            if ($tura == $id_gracza && $iloscGraczy == 2) {
+                $plansza = mysqli_fetch_row(mysqli_query($conn, "SELECT plansza FROM pomieszczenia WHERE id = $id_pomieszczenia"))[0];
+                $sql = "SELECT id_gracza1 FROM pomieszczenia WHERE id = $id_pomieszczenia";
+                if($id_gracza == mysqli_fetch_row(mysqli_query($conn, $sql))[0]) {
+                    $znak = "O";
+                } else {
+                    $znak = "X";
+                }
+                if ($plansza[(int)$nrPrzycisku] == '0') {
+                    $plansza[(int)$nrPrzycisku] = $znak;
+                    $sql = "UPDATE pomieszczenia SET plansza = '$plansza' WHERE id = $id_pomieszczenia";
+                    mysqli_query($conn, $sql);
+                    echo $nrPrzycisku;
+                    echo $plansza;
+                    $sql = "UPDATE pomieszczenia SET tura = $id_gracza_2 WHERE id = $id_pomieszczenia";
+                    mysqli_query($conn, $sql);
+                    echo "<script> document.getElementById('przycisk$nrPrzycisku').setAttribute('value','$znak')</script>";
+                    echo "<script> document.getElementById('przycisk$nrPrzycisku').setAttribute('disabled','true')</script>";
+                }
+                
+                
+                
+            }
+
+        }
+
+        if (isset($_POST["przycisk"])) {
+            $nrPrzycisku = $_POST["przycisk"];
+            klikPrzycisku($nrPrzycisku);
+
+        }
+    ?>
+
 </body>
 </html>
 <?php
--- index.php
+++ index.php
@@ -3,6 +3,24 @@
     if (isset($_POST["play"])) {
         $sql = "INSERT INTO gracze ()  VALUES ()";
         mysqli_query($conn, $sql);
+        $sql = 'SELECT MIN(liczba_graczy) FROM pomieszczenia';
+        $minGraczy = mysqli_fetch_row(mysqli_query($conn, $sql))[0];
+        $sql = "SELECT id FROM gracze ORDER BY id DESC LIMIT 1";
+        $id_gracza = mysqli_fetch_row(mysqli_query($conn, $sql))[0];
+        if ($minGraczy == 1) {
+            $sql = "SELECT id FROM pomieszczenia WHERE liczba_graczy = 1";
+            $id_pomieszczenia = mysqli_fetch_row(mysqli_query($conn, $sql))[0];
+            $sql = "UPDATE pomieszczenia SET liczba_graczy = 2, id_gracza2 = $id_gracza WHERE id = $id_pomieszczenia";
+            mysqli_query($conn, $sql);
+        } else {
+            $sql = "INSERT INTO pomieszczenia (id_gracza1, liczba_graczy, tura) VALUES ($id_gracza, 1, $id_gracza)";
+            mysqli_query($conn, $sql);
+            $sql = "SELECT id FROM pomieszczenia WHERE liczba_graczy = 1";
+            $id_pomieszczenia = mysqli_fetch_row(mysqli_query($conn, $sql))[0];
+        }
+        setcookie("idGracza", $id_gracza, time() + (86400 * 30), "/");
+        setcookie("idPomieszczenia", $id_pomieszczenia, time() + (86400 * 30), "/");
+
         header("Location: game.php");
         exit();
     }
